540a3a8dd8ca43f14f711ddf0c2c588ab6fcc9aa
builtbyjosh <pc.refuge@gmail.com>
2023-02-02 10:59:21 -0500

    add about page

---
+++ pages/about/index.tsx
@@ -0,0 +1,36 @@
+import React from 'react';
+import { Box, Text, Heading } from '@chakra-ui/react';
+
+const index = () => {
+  return (
+    <Box>
+      <Heading>About Us</Heading>
+      <Text>
+        LeTub Saloon has been safely tucked away on A1A behind lush greenery,
+        that even people who have lived in Hollywood for years, have unknowingly
+        driven right past. For the last 32 years it has been a refuge to a large
+        handful of lucky locals, whom either found it while boating on the
+        Intercoastal Waterway or informed by word of mouth, for there were no
+        signs and no advertising.
+      </Text>
+      <br/>
+      <Text>
+        LeTub has been impressing people for many years and for many reasons.The
+        food(all of it)has always been outstanding, no matter what you choose to
+        eat.We offer many one of a kind recipes you won’t find anywhere else,
+        including our Seafood Gumbo the House Salad Dressing, and Key Lime Pie
+        that has always been made on premisesand the recipes have never been
+        differed.The only thing that has differed is our recent claim to fame in
+        2006 when GQ gave our Burger #1 in the nation. We know what a honor that
+        is and truly appreciate it, yet we believe that LeTub should remain
+        impressing people for the same reasons it always has. And for the people
+        who appreciate the food and atmosphere that took years and hard work to
+        establish, and so many come here to enjoy.We surly hope that if you have
+        some extra time on your hands you will come and enjoy what the locals
+        already get…..A little slice of Heaven on Hollywood Beach
+      </Text>
+    </Box>
+  );
+};
+
+export default index;
